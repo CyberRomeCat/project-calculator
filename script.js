cb03dd4c9c96f1992a8577743ff799b7f6fd4d95
jenzie <jacecabalo@gmail.com>
2023-03-21 09:37:10 +0400

    all files

---
+++ script.js
@@ -0,0 +1,22 @@
+let number = 5 ;
+let operator = *+
+let AnoNum  = 2;
+
+
+
+function add(x,y) {
+    return x + y;
+}
+
+function subtract(x,y) {
+    return x - y;
+}
+
+function multiply(x,y) {
+    return x*y
+}
+
+function divide(x,y) {
+    return x/y
+}
+
