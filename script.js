5e4a38f4de3a3c9ca7b5f9afa5ec1d846dce283f
jenzie <jacecabalo@gmail.com>
2023-03-25 12:25:41 +0400

    25-03-23

--- script.js
+++ script.js
@@ -1,22 +1,96 @@
-let number = 5 ;
-let operator = *+
-let AnoNum  = 2;
+let numBtns = document.querySelectorAll('.num');
+let opBtns = document.querySelectorAll('.op');
+const resetBtn = document.getElementById('reset');
+let eqBtn = document.querySelector('.equal');
 
+opBtns.forEach(op => op.addEventListener('click',getOperate));
+numBtns.forEach(num => num.addEventListener('click',getNum));
+resetBtn.addEventListener('click',() => {
+    num = '';
+    inputNum = '0';
+    operator = '';
+    inputOper = '';
+    display.innerText = num;
+});
+eqBtn.addEventListener('click',() => {
 
+        inputNum = operate(operator,inputNum,num);
+        display.innerText = inputNum;
+        num = '';
+});
 
-function add(x,y) {
-    return x + y;
+let num = '';
+let inputNum = '0';
+let operator = '';
+let inputOper = '';
+
+function getOperate() { 
+    inputOper = operator;
+    operator = this.innerText;  
+
+    if (inputOper === '' && (operator === '/' || '-')) {
+        inputNum = num;
+        display.innerText = inputNum;
+        num = '';
+    } else if (inputOper === '' && (operator === '*')) {
+        inputNum = 1;
+        inputNum = operate(operator,inputNum,num);
+        display.innerText = inputNum;
+        num = '';
+    } else if (inputOper === '') {
+        inputNum = operate(operator,inputNum,num);
+        display.innerText = inputNum;
+        num = '';
+    } else {
+        inputNum = operate(inputOper,inputNum,num);
+        display.innerText = inputNum;
+        num = '';
+    };
+};
+
+function getNum() {
+    num = num.concat(this.innerText);
+    display.innerText = num;
 }
 
-function subtract(x,y) {
-    return x - y;
-}
+//operator functions
 
-function multiply(x,y) {
-    return x*y
-}
+function add(...args) {
+    let sum = 0;
+    for (let arg of args) sum += parseFloat(arg);
+    return sum;
+};
 
-function divide(x,y) {
-    return x/y
-}
+function subtract(...args) {
+    let array = [...args];
+    const subtract = array.reduce((a, b) => a - b);
+
+    return subtract;
+};
+
+function multiply(...args) {
+    let sum = 1;
+    for (let arg of args) sum *= parseFloat(arg);
+    return sum;
+};
+
+function divide(...args) {
+    let array = [...args];
+    const division = array.reduce((a, b) => a / b);
+
+
+    return division;
+};
+
+function operate(op,...nums) {
+      if (op === '-') {
+        return subtract(...nums)
+    }  if (op === '*') {
+        return multiply(...nums)
+    } if (op === '/') {
+        return divide(...nums)
+    } if (op === '+') {
+        return add(...nums)
+    }
+};
 
